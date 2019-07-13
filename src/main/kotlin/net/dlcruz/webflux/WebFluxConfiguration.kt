54dcc56f1077b8f364b4c3a27b20fb88f55bc026
sesinando2 <sesinando2@gmail.com>
2019-07-13 21:24:11 +1000

    Add WebFlux Configuration (#5)
    * #4 Add WebFlux Configuration
    
    * #4 ktlint
    
    * #4 trigger build
    
    * #4 Fix failing test
    
    * #4 Remove unused

---
+++ src/main/kotlin/net/dlcruz/webflux/WebFluxConfiguration.kt
@@ -0,0 +1,13 @@
+package net.dlcruz.webflux
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.codec.ServerCodecConfigurer
+import org.springframework.web.reactive.config.WebFluxConfigurer
+
+@Configuration
+class WebFluxConfiguration : WebFluxConfigurer {
+
+    override fun configureHttpMessageCodecs(configurer: ServerCodecConfigurer) {
+        configurer.defaultCodecs().enableLoggingRequestDetails(true)
+    }
+}
