f16655e6dd9b5a526cc4e433f437a7b29be8de1f
Nando Dela Cruz <sesinando2@gmail.com>
2019-07-13 20:44:16 +1000

    #4 Fix failing test

--- src/main/kotlin/net/dlcruz/webflux/WebFluxConfiguration.kt
+++ src/main/kotlin/net/dlcruz/webflux/WebFluxConfiguration.kt
@@ -6,7 +6,6 @@ import org.springframework.web.reactive.config.EnableWebFlux
 import org.springframework.web.reactive.config.WebFluxConfigurer
 
 @Configuration
-@EnableWebFlux
 class WebFluxConfiguration : WebFluxConfigurer {
 
     override fun configureHttpMessageCodecs(configurer: ServerCodecConfigurer) {
