b6ac88f946bbc5babd0e855520ffba828228f375
Nando Dela Cruz <sesinando2@gmail.com>
2019-07-13 19:17:05 +1000

    #4 Add WebFlux Configuration

---
+++ src/main/kotlin/net/dlcruz/webflux/WebFluxConfiguration.kt
@@ -0,0 +1,15 @@
+package net.dlcruz.webflux
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.codec.ServerCodecConfigurer
+import org.springframework.web.reactive.config.EnableWebFlux
+import org.springframework.web.reactive.config.WebFluxConfigurer
+
+@Configuration
+@EnableWebFlux
+class WebFluxConfiguration: WebFluxConfigurer {
+
+    override fun configureHttpMessageCodecs(configurer: ServerCodecConfigurer) {
+        configurer.defaultCodecs().enableLoggingRequestDetails(true)
+    }
+}
